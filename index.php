096b8b354d0b4b6b527433a2c46073d846dc1029
camiloperez17 <a17campermor@inspedralbes.cat>
2022-03-09 09:12:16 +0100

    update index

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Docker php</title>
+    <title>Docker amb php</title>
 </head>
 <body>
     <?php
-        echo "Hola soc Camilo Perez";
+    echo "Hola soc Camilo Pérez Morán amb php";
     ?>
 </body>
 </html>
